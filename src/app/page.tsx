d61a98c5b6253bf2abc422b6fbb1dde6a0113169
Sean Gransee <sean.gransee@gmail.com>
2025-09-22 14:58:49 +0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1
+        className="text-6xl md:text-8xl text-black text-center"
+        style={{ fontFamily: '"Brush Script MT", "Lucida Handwriting", "Comic Sans MS", cursive' }}
+      >
+        Sean + Lexi = Sexi
+      </h1>
+    </div>
+  );
+}
