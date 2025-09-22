e75efebb09ea5e44fd7a2d4fa0cadb76d4f392d9
Sean Gransee <sean.gransee@gmail.com>
2025-09-22 15:13:05 +0700

    Add favicon images and update layout to include them in head.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${dancingScript.variable} antialiased`}
       >
